2127eb2a7a87d2a468f33605bf7b9cf7059c9771
Musical-Bonesie <83307690+Musical-Bonesie@users.noreply.github.com>
2021-08-14 19:18:08 -0600

    added product carousel and working button

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Header/index";
 import HomePage from "./pages/HomePage/index";
+import ProductsCarousel from "./components/ProductsCarousel/ProductsCarousel";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/products" exact component={HomePage} />
+          <Route path="/carousel" exact component={ProductsCarousel} />
           {/* <Route path ="/login" exact component={Login} /> */}
           {/* <ProtectedRoute to="/" exact component={HomePage} /> */}
         </Switch>
