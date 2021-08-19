918069866db621f545e9a0a4a6e7aef3e8ca5526
Musical-Bonesie <83307690+Musical-Bonesie@users.noreply.github.com>
2021-08-19 12:14:58 -0600

    DB user can add products to their yesSensitivity list

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -3,7 +3,7 @@ const uuid = require("uuid");
 const fs = require("fs");
 //for Prisma
 const { PrismaClient } = require("@prisma/client");
-const { user, noSensitivity } = new PrismaClient();
+const { user, noSensitivity, yesSensitivity, products } = new PrismaClient();
 //GET user info
 async function getUsers(req, res) {
   // const users = usersModel.getUser();
@@ -28,33 +28,23 @@ function getSingleUser(req, res) {
 }
 //Add or Patch products to a user
 //TODO add to the no_sensitivity list/Yes Products figure out why this isnt't working..
-function addNoSensitivity(req, res) {
-  const users = usersModel.getUser();
-  console.log(users);
-  const singleUser = users.find((user) => req.params.userID === user.id);
-  console.log(singleUser);
+// function addNoSensitivity(req, res) {
+//   const users = usersModel.getUser();
+//   console.log(users);
+//   const singleUser = users.find((user) => req.params.userID === user.id);
+//   console.log(singleUser);
 
-  singleUser.no_sensitivity.push(req.body);
-  console.log(singleUser);
-  console.log(users);
-  fs.writeFileSync("./data/users.json", JSON.stringify(users));
-  //TODO try this usersModel.setUser(users);
-  res.status(201).json(req.body);
-}
-/////test///
+//   singleUser.no_sensitivity.push(req.body);
+//   console.log(singleUser);
+//   console.log(users);
+//   fs.writeFileSync("./data/users.json", JSON.stringify(users));
+//   //TODO try this usersModel.setUser(users);
+//   res.status(201).json(req.body);
+// }
+/////test Add product to a user's noSensitivity list ///
 async function addNotSensitiveTo(req, res) {
   console.log(req.body);
-  const {
-    id,
-    brandName,
-    productName,
-    ingredients,
-    image,
-    userId,
-    price,
-    category,
-    status,
-  } = req.body;
+  const { id, brandName, productName, ingredients, image, userId } = req.body;
 
   const userExists = await user.findUnique({
     where: {
@@ -75,9 +65,8 @@ async function addNotSensitiveTo(req, res) {
       productName,
       ingredients,
       image,
-      price,
-      category,
-      status,
+
+      // add/connect this product to this user
       user: {
         connect: {
           id: userId,
@@ -88,33 +77,63 @@ async function addNotSensitiveTo(req, res) {
           id: id,
         },
       },
-      // userId: req.body.userId,
     },
   });
   console.log(addProduct);
   res.json(addProduct);
 }
-////test///
 
-//user can add products they ARE sensitive to:
-function addSensitiveTo(req, res) {
-  const users = usersModel.getUser();
-  console.log(users);
-  const singleUser = users.find((user) => req.params.userID === user.id);
-  console.log(singleUser);
-
-  singleUser.yes_sensitivity.push(req.body);
-  console.log(singleUser);
-  console.log(users);
-  fs.writeFileSync("./data/users.json", JSON.stringify(users));
-  //TODO try this usersModel.setUser(users);
-  res.status(201).json(req.body);
+//DB - User adds a product they are sensitive to to the yesSensitivity list. If it already exsits it won't be added to their list.
+async function addSensitiveTo(req, res) {
+  console.log(req.body);
+  const { id, userId, brandName, productName, ingredients, image } = req.body;
+  const userExists = await user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+  if (!userExists) {
+    return res.status(400).json({
+      msg: "user not found",
+    });
+  }
+  console.log(userExists);
+  const returnProduct = req.body;
+  //
+  const productAlreadyExists = await yesSensitivity.findFirst({
+    where: {
+      products: {
+        some: { id: id },
+      },
+    },
+  });
+  console.log(productAlreadyExists);
+  if (!productAlreadyExists) {
+    const addProduct = await yesSensitivity.create({
+      data: {
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+        products: {
+          connect: {
+            id: id,
+          },
+        },
+      },
+    });
+    res.status(201).json(returnProduct);
+  } else {
+    return res.json({
+      msg: "product already exsits on this list",
+    });
+  }
 }
-
 module.exports = {
   getUsers,
   getSingleUser,
-  addNoSensitivity,
+  // addNoSensitivity,
   addSensitiveTo,
   addNotSensitiveTo,
 };
