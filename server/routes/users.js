5d8e3cb28921d126e92a9d51ec72ee3668e31421
Musical-Bonesie <83307690+Musical-Bonesie@users.noreply.github.com>
2021-08-18 23:39:07 -0600

    created rough DB and server route to add notSensitive to

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,35 +7,53 @@ const public = path.join(__dirname, "public");
 const jwt = require("jsonwebtoken"); //jwt libary installed
 const bcrypt = require("bcryptjs"); // means within server directory ablity to encrypt passwords
 const authorize = require("../middleware/authorize"); // middleware to check there is an authroization header sent
-//delete line 9 and 10 and make sure it's inside the userController
+//TODO delete line 9 and 10 and make sure it's inside the userController
 const { PrismaClient } = require("@prisma/client");
 const { user } = new PrismaClient();
 
 ///.get all users is via prisma
 router.get("/", usersController.getUsers);
+///////
 router.get("/:id", usersController.getSingleUser);
 
-//POST Add a user remeber to add Auser Auth //TODO change this to add more user info
+//POST Add a user remeber to add userAuth //TODO change this to add more user info
 router.post("/", async (req, res) => {
   console.log(req.body);
   const { username } = req.body;
-  if (username == null) {
-    res.status(500).send("username undefined");
-  } else {
-    //check to see if user already exsits
-    const userExists = await user.findUnique({
-      where: { username },
-      select: {
-        username: true,
-      },
+  // if (username == null) {
+  //   res.status(500).send("username undefined");
+  // } else {
+  //check to see if user already exsits
+  const userExists = await user.findUnique({
+    where: { username },
+    select: {
+      username: true,
+    },
+  });
+  if (userExists) {
+    return res.status(400).json({
+      msg: "user already exists",
     });
-    res.json(userExists);
   }
+  //Create a new user
+  const newUser = await user.create({
+    data: {
+      username: req.body.username,
+      password: req.body.password,
+      //TODO token          String
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+    },
+  });
+  res.json(newUser);
 });
 
 //PATCH
 //user adds a product that they are NOT sensitive to:
-router.patch("/:userID", usersController.addNoSensitivity);
+// router.patch("/:userID", usersController.addNoSensitivity);
+router.patch("/:userID", usersController.addNotSensitiveTo);
+
 //user adds product that they ARE sensitive to:
 router.patch("/sensitive/:userID", usersController.addSensitiveTo);
 
