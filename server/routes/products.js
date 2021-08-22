99f0602a47d588fa395b0235698c1a1e7f4fd21a
Musical-Bonesie <83307690+Musical-Bonesie@users.noreply.github.com>
2021-08-22 02:06:56 -0600

    trying to create and debug a delete function for user to remove yesSensitivity product

--- server/routes/products.js
+++ server/routes/products.js
@@ -6,33 +6,4 @@ const public = path.join(__dirname, "public");
 //GET
 router.get("/", productsController.getProducts);
 
-//Test Post so instead this product
-//this post can't be create without the user
-router.post("/", async (req, res) => {
-  //need to provide user id so it can be associated with this
-  const { title, content, user_id } = req.body;
-  //check if user exsits
-  const userExists = await userExists.findUnique({
-    where: {
-      id: user_id,
-    },
-  });
-  //if user does NOT exist
-  if (!userExists) {
-    return res.status(400).json({
-      meg: "user not found",
-    });
-  }
-  //creating a new post
-  const newPost = await this.post.update({
-    data: {
-      title,
-      //column name is post
-      post: conten,
-      user_id,
-    },
-  });
-  res.json(newPost);
-});
-
 module.exports = router;
