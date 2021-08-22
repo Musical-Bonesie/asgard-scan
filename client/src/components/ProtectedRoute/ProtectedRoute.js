91a8ba096b1f91603dd0fc989097f0b29ef119c4
Musical-Bonesie <83307690+Musical-Bonesie@users.noreply.github.com>
2021-08-22 13:30:03 -0600

    added ProtectedRoute

--- client/src/components/ProtectedRoute/ProtectedRoute.js
+++ client/src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-/// TODO Set this up once DB is set up
-const ProtectedRoute = ({ component: Component, ...rest }) => {
-  //had { ... reactRouterProps} instead of ...rest --> this shows token that's inside of sessionSotrage if user has logged in.
+import HomePage from "../../pages/HomePage/HomePage";
 
+//create a route so only valid users can access the HomePage.
+// ...rest gives access to token inside of sessionStorage if a valid user has logged in.
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
   const token = sessionStorage.getItem("token");
-  //return a Route that contains all of the REACT router props.
+  //return a Route that contains all reactRouterProps to render a specific page depending if there is a valid token or not.
   return (
     <Route
-      // {...rest }can change to {...reactRouterProps} -->
       {...rest}
       render={(routeProps) =>
-        //inside of this callbackFunc it checks to see if there is a token in sessionStorage or not.
-        //if there is do something/login, if not redirct.
-        !token ? <Redirect to="/login" /> : <Component {...routeProps} />
+        //If there is no token rediect to login page. If there is a valid token take the user to the HomePage.
+        !token ? <Redirect to="/login" /> : <HomePage {...routeProps} />
       }
     />
   );
